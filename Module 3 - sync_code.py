2a123fda4dac6ccc19d754793f175094d465dbde
Sarah Martin <sarahlesliem21@gmail.com>
2019-01-28 22:13:38 -0500

    Cleans up time testing
    cleans up time testing function (uses a single test function), but runs it for 5 different time capture functions to try and clarify why results are not as expected.  CPU time-based time capture functions result in 0 ms run times pretty consistently.  Either my computer is too fast, or it's internal clock ticks too slowly

--- Module 3 - sync_code.py
+++ Module 3 - sync_code.py
@@ -21,7 +21,7 @@ fact_rec(5)
 fact_iter(5)
 
 # Generates test data
-# Chose integers from 100-900 instead of 100-500 to show greater time differences
+# Chose integers from 100-950 instead of 100-500 to show greater time differences
 random.seed(5)
 test_data = [random.randint(100, 950) for _ in range(10)]
 test_data.sort()
@@ -31,94 +31,68 @@ test_data
 sys.getrecursionlimit()
 ## sys.setrecursionlimit(5000) DANGEROUS!!
 
-# VERSION 1: time tests each function once for each test_int value
-#Initializes Results Data Capture Lists
-fact_results_v1 = []
-recur_time_results_v1 = []
-iter_time_results_v1 = []
-
-# Captures factorial results & function run times
-for test_int in test_data:
-    #print("Factorial of", test_int)
-    fact_results_v1.append(fact_iter(test_int))
-
-    time.sleep(1)
-    rec_start = time.time()
-    fact_rec(test_int)
-    rec_end = time.time()
-    #print("Recursive Method Time: ", ((end - start)/1000000), " ms")
-    recur_time_results_v1.append((rec_end - rec_start)*1000)
-
-    time.sleep(1)
-    iter_start = time.time()
-    fact_iter(test_int)
-    iter_end = time.time()
-    #print("For-Loop Method Time: ", ((end - start)/1000000), " ms\n")
-    iter_time_results_v1.append((iter_end - iter_start)*1000)
-
-
-# time results were too variable/weird from run to run, so decided to run 10 times and average
-# VERSION 2: time tests each function 10 times for each test_int value and averages the times recorded
-fact_results_v2 = []
-recur_time_results_v2 = []
-iter_time_results_v2 = []
-
-# Captures factorial results & function run times
-for test_int in test_data:
-    #print("Factorial of", test_int)
-    fact_results_v2.append(fact_iter(test_int))
-
-    rec_temp_time_results = []
-    for i in range(10):
-        time.sleep(1)
-        rec_start = time.time()
-        fact_rec(test_int)
-        rec_end = time.time()
-        rec_temp_time_results.append((rec_end - rec_start)*1000)
-    
-    #print("Recursive Method Time: ", ((end - start)/1000000), " ms")
-    recur_time_results_v2.append(sum(rec_temp_time_results)/len(rec_temp_time_results))
-
-    iter_temp_time_results = []
-    for i in range(10):
-        time.sleep(1)
-        iter_start = time.time()
-        fact_iter(test_int)
-        iter_end = time.time()
-        iter_temp_time_results.append((iter_end - iter_start)*1000)
-    
-    #print("For-Loop Method Time: ", ((end - start)/1000000), " ms\n")
-    iter_time_results_v2.append(sum(iter_temp_time_results)/len(iter_temp_time_results))
-
-# VERSION 3: thanks ethan
-def test_loop(f):
+# Creates time test function applicable with various different time capture functions
+def test_loop(f,t):
     fact_results = []
     time_results = []
     for test_int in test_data:
-        start = time.perf_counter()
+        start = t()
         fact = f(test_int)
-        end = time.perf_counter()
+        end = t()
         fact_results.append(fact)
-        time_results.append(end - start)
+        time_results.append((end - start)*1000)
     return fact_results, time_results
 
-recur_fact_results_v3, recur_time_results_v3 = test_loop(fact_rec)
-iter_fact_results_v3, iter_time_results_v3 = test_loop(fact_iter)
 
-# Examines results
-recur_fact_results_v3 == iter_fact_results_v3
-fact_results_v1 == fact_results_v2
+# VERSION 1: Uses regular time.time() function
+recur_fact_results_vtime, recur_time_results_vtime = test_loop(fact_rec, time.time)
+iter_fact_results_vtime, iter_time_results_vtime = test_loop(fact_iter, time.time)
 
-recur_time_results_v1
-iter_time_results_v1
+# Examines Version 1 results
+recur_fact_results_vtime == iter_fact_results_vtime
+recur_time_results_vtime
+iter_time_results_vtime
 
-recur_time_results_v2
-iter_time_results_v2
 
-recur_time_results_v3
-iter_time_results_v3
+# VERSION 2: Uses time.thread_time() function
+recur_fact_results_vthread, recur_time_results_vthread = test_loop(fact_rec, time.thread_time)
+iter_fact_results_vthread, iter_time_results_vthread = test_loop(fact_iter, time.thread_time)
 
-#print (result_rec == result_iter)
+# Examines Version 2 results
+recur_fact_results_vthread == iter_fact_results_vthread
+recur_time_results_vthread
+iter_time_results_vthread
+
+
+# VERSION 3: Uses time.monotonic() function
+recur_fact_results_vmonotonic, recur_time_results_vmonotonic = test_loop(fact_rec, time.monotonic)
+iter_fact_results_vmonotonic, iter_time_results_vmonotonic = test_loop(fact_iter, time.monotonic)
+
+# Examines Version 3 results
+recur_fact_results_vmonotonic == iter_fact_results_vmonotonic
+recur_time_results_vmonotonic
+iter_time_results_vmonotonic
+
+
+# VERSION 4: Uses time.process_time() function
+recur_fact_results_vprocess, recur_time_results_vprocess = test_loop(fact_rec, time.process_time)
+iter_fact_results_vprocess, iter_time_results_vprocess = test_loop(fact_iter, time.process_time)
+
+# Examines Version 3 results
+recur_fact_results_vprocess == iter_fact_results_vprocess
+recur_time_results_vprocess
+iter_time_results_vprocess
+
+
+# VERSION 5: Uses time.perf_counter() function
+recur_fact_results_vperfCounter, recur_time_results_vperfCounter = test_loop(fact_rec, time.perf_counter)
+iter_fact_results_vperfCounter, iter_time_results_vperfCounter = test_loop(fact_iter, time.perf_counter)
+
+# Examines Version 3 results
+recur_fact_results_vperfCounter == iter_fact_results_vperfCounter
+recur_time_results_vperfCounter
+iter_time_results_vperfCounter
+
 
 # In this Extra Credit Assignment, 
 # please modify the recursive factorial function 
