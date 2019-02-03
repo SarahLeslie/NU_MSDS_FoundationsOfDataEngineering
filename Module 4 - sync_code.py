e528832f361df6af7e067126fb825a4aadb53279
Sarah Martin <sarahlesliem21@gmail.com>
2019-02-03 16:04:47 -0500

    cleans up starter code
    removes unnecessary lines, re-organizes, & re-labels code

--- Module 4 - sync_code.py
+++ Module 4 - sync_code.py
@@ -1,44 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 29 21:07:05 2019
+# Imports required packages
+import sys
+import Person_pb2
+import binascii
 
-@author: dev2
-"""
-
+# Defines quicksort function
 def quicksort(array):
-    
-  print("Sorting array: {0}".format(array))  
-    
+  
+  # base case, arrays with 0 or 1 element are already "sorted"
   if len(array) < 2:
-    # base case, arrays with 0 or 1 element are already "sorted"
     return array
+
   else:
-    # recursive case
+    # assigns pivot element
     pivot = array[0]
-    # sub-array of all the elements less than the pivot
+    # creates sub-array of all the elements less than the identified pivot
     less = [i for i in array[1:] if i <= pivot]
-    # sub-array of all the elements greater than the pivot
+    # creates sub-array of all the elements greater than the identified pivot
     greater = [i for i in array[1:] if i > pivot]
-    
-    print("{0} <{1}> {2}".format(less, pivot, greater))
-    
+    # recursive call of quicksort on the less-than and greater-than arrays (nesting the pivot in between)
     return quicksort(less) + [pivot] + quicksort(greater)
 
-print(quicksort([10, 5, 2, 13, 7, 8, 1, 15, 12, 8]))
-
-
-
-import sys
-sys.path.append('C:/Users/dev2/Documents/Teaching/Northwestern/Instruction/2019WI_MSDS_432-DL_SEC56/2019WI_MSDS_432-DL_SEC56/module4/')
-import Person_pb2
-
+# Creates test person object with username, fav number, and interest attributes
 person_obj = Person_pb2.Person()
 person_obj.user_name = 'Martin'
 person_obj.favorite_number = 1337
 person_obj.interests.extend(['daydreaming','hacking'])
 
-import binascii
-
+# Not 100% sure yet what this does but i'll figure it out!
 binascii.hexlify(person_obj.SerializeToString())
 person_obj.SerializeToString()
 
