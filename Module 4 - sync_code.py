85198897af582f07c8969e4cacc0463f07c9ff7a
Sarah Martin <sarahlesliem21@gmail.com>
2019-02-04 02:08:46 -0500

    troubleshoots and prepares testing
    Ammends various sort function to accept a list of dictionaries, and a key (found in all dictionaries).  Sort functions then sort the whole list of dictionaries, as if they are rows in a dataframe based on the key (~dataframe column name).  Also adds time testing and time testing results capture

--- Module 4 - sync_code.py
+++ Module 4 - sync_code.py
@@ -1,47 +1,45 @@
 # IMPORTS REQUIRED PACKAGES
 import random
 import string
+import time
 import pandas as pd
-#import sys
-#import Person_pb2
-#import binascii
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # DEFINES VARIOUS SORT FUNCTIONS
 # Defines quicksort function
-def quicksort(array):
-  
+def quicksort(list_of_dicts, key):  
   # base case, arrays with 0 or 1 element are already "sorted"
-  if len(array) < 2:
-    return array
-
+  if len(list_of_dicts) < 2:
+    return list_of_dicts
   else:
     # assigns pivot element
-    pivot = array[0]
+    pivot_element = list_of_dicts[0]
     # creates sub-array of all the elements less than the identified pivot
-    less = [i for i in array[1:] if i <= pivot]
+    less = [i for i in list_of_dicts[1:] if i[key] <= pivot_element[key]]
     # creates sub-array of all the elements greater than the identified pivot
-    greater = [i for i in array[1:] if i > pivot]
+    greater = [i for i in list_of_dicts[1:] if i[key] > pivot_element[key]]
     # recursive call of quicksort on the less-than and greater-than arrays (nesting the pivot in between)
-    return quicksort(less) + [pivot] + quicksort(greater)
+    return quicksort(less,key) + [pivot_element] + quicksort(greater,key)
 
 # Uses version of selection sort function from Week 2
-def selectionSort(arr):
-  for i in range(len(arr)): 
+def selectionSort(list_of_dicts, key):
+  for i in range(len(list_of_dicts)): 
     min_index = i 
-    min_value = arr[i]
+    min_value = list_of_dicts[i][key]
     # cycles through remaining array to find next min value
-    for j in range(i+1, len(arr)): 
-        if arr[j] < min_value: 
+    for j in range(i+1, len(list_of_dicts)): 
+        if list_of_dicts[j][key] < min_value: 
             min_index = j 
-            min_value = arr[j]
+            min_value = list_of_dicts[j][key]
     # Swap the first element and the minimum element within the remaining unsorted portion of the array
-    arr[i], arr[min_index] = arr[min_index], arr[i] 
-  return arr
+    list_of_dicts[i], list_of_dicts[min_index] = list_of_dicts[min_index], list_of_dicts[i] 
+  return list_of_dicts
 
 # Defines Bubble sort function (from geeksforgeeks.org)
-def bubbleSort(arr): 
-  n = len(arr) 
+def bubbleSort(list_of_dicts,key): 
+  n = len(list_of_dicts) 
   # Traverse through all array elements 
   for i in range(n): 
       # Last i elements are already in place 
@@ -49,31 +47,31 @@ def bubbleSort(arr):
           # traverse the array from 0 to n-i-1 
           # Swap if the element found is greater 
           # than the next element 
-          if arr[j] > arr[j+1] : 
-              arr[j], arr[j+1] = arr[j+1], arr[j] 
-  return arr
+          if list_of_dicts[j][key] > list_of_dicts[j+1][key] : 
+              list_of_dicts[j], list_of_dicts[j+1] = list_of_dicts[j+1], list_of_dicts[j] 
+  return list_of_dicts
 
 # Defines Insertion sort function (from geeksforgeeks.org)
-def insertionSort(arr): 
+def insertionSort(list_of_dicts, key): 
   # Traverse through 1 to len(arr) 
-  for i in range(1, len(arr)): 
-      key = arr[i] 
+  for i in range(1, len(list_of_dicts)): 
+      comp_element = list_of_dicts[i]
       # Move elements of arr[0..i-1], that are 
       # greater than key, to one position ahead 
       # of their current position 
       j = i-1
-      while j >=0 and key < arr[j] : 
-              arr[j+1] = arr[j] 
+      while j >=0 and comp_element[key] < list_of_dicts[j][key] : 
+              list_of_dicts[j+1] = list_of_dicts[j] 
               j -= 1
-      arr[j+1] = key 
-  return arr
+      list_of_dicts[j+1] = comp_element 
+  return list_of_dicts
 
 # Defines Strand sort function (drafted myself from online pseudo-code)
 # Defines merge of pre-sorted lists first
-def my_merge(l1, l2):
+def my_merge(l1, l2, key):
   result = []
   while (l1 and l2):
-    if (l1[0] <= l2[0]):
+    if (l1[0][key] <= l2[0][key]):
         result.append(l1.pop(0))
     else:
         result.append(l2.pop(0))
@@ -81,53 +79,64 @@ def my_merge(l1, l2):
   result.extend(l1 if l1 else l2)
   return result
 
-def strandSort(arr):
-  if len(arr) < 2:
-    return arr 
-  result_arr = []
-  while len(arr)>0:
+def strandSort(list_of_dicts,key):
+  if len(list_of_dicts) < 2:
+    return list_of_dicts 
+  result_list_of_dicts = []
+  while len(list_of_dicts)>0:
     i = 0
-    #sublist = []
-    sublist = [arr.pop(0)]
-    while i < len(arr):
-      if arr[i] > sublist[-1]:
-        sublist.append(arr.pop(i))
+    sublist = [list_of_dicts.pop(0)]
+    while i < len(list_of_dicts):
+      if list_of_dicts[i][key] > sublist[-1][key]:
+        sublist.append(list_of_dicts.pop(i))
       else:
         i = i + 1
-    result_arr = my_merge(sublist,result_arr)
-  return result_arr
+    result_list_of_dicts = my_merge(sublist,result_list_of_dicts,key)
+  return result_list_of_dicts
 
 # Defines Gnome sort function (from geeksforgeeks.org with slight tweaks)
-def gnomeSort(arr): 
+def gnomeSort(list_of_dicts,key): 
   index = 1
-  while index < len(arr): 
+  while index < len(list_of_dicts): 
     if index == 0: 
         index = index + 1
-    if arr[index] >= arr[index - 1]: 
+    if list_of_dicts[index][key] >= list_of_dicts[index - 1][key]: 
         index = index + 1
     else: 
-        arr[index], arr[index-1] = arr[index-1], arr[index] 
+        list_of_dicts[index], list_of_dicts[index-1] = list_of_dicts[index-1], list_of_dicts[index] 
         index = index - 1
-  return arr 
+  return list_of_dicts 
 
 # Functional testing of quicksort, bubblesort, gnomesort, insertionsort, selectionsort, and strandsort functions
-test_list = [1,10,6,5,2,9,7,4,3,8]
-quicksort(test_list)
+test_list_of_dicts = [{'first_name':'Sarah', 'last_name':'Martin', 'city':'NYC'}
+                       , {'first_name':'Rachel', 'last_name':'Martin', 'city':'Berlin'}
+                       , {'first_name':'David', 'last_name':'Martin', 'city':'Stamford'}]
+quicksort(test_list_of_dicts,'city')
 
-test_list = [1,10,6,5,2,9,7,4,3,8]
-bubbleSort(test_list)
+test_list_of_dicts = [{'first_name':'Sarah', 'last_name':'Martin', 'city':'NYC'}
+                       , {'first_name':'Rachel', 'last_name':'Martin', 'city':'Berlin'}
+                       , {'first_name':'David', 'last_name':'Martin', 'city':'Stamford'}]
+bubbleSort(test_list_of_dicts,'city')
 
-test_list = [1,10,6,5,2,9,7,4,3,8]
-gnomeSort(test_list)
+test_list_of_dicts = [{'first_name':'Sarah', 'last_name':'Martin', 'city':'NYC'}
+                       , {'first_name':'Rachel', 'last_name':'Martin', 'city':'Berlin'}
+                       , {'first_name':'David', 'last_name':'Martin', 'city':'Stamford'}]
+gnomeSort(test_list_of_dicts,'city')
 
-test_list = [1,10,6,5,2,9,7,4,3,8]
-insertionSort(test_list)
+test_list_of_dicts = [{'first_name':'Sarah', 'last_name':'Martin', 'city':'NYC'}
+                       , {'first_name':'Rachel', 'last_name':'Martin', 'city':'Berlin'}
+                       , {'first_name':'David', 'last_name':'Martin', 'city':'Stamford'}]
+insertionSort(test_list_of_dicts,'city')
 
-test_list = [1,10,6,5,2,9,7,4,3,8]
-selectionSort(test_list)
+test_list_of_dicts = [{'first_name':'Sarah', 'last_name':'Martin', 'city':'NYC'}
+                       , {'first_name':'Rachel', 'last_name':'Martin', 'city':'Berlin'}
+                       , {'first_name':'David', 'last_name':'Martin', 'city':'Stamford'}]
+selectionSort(test_list_of_dicts,'city')
 
-test_list = [1,10,6,5,2,9,7,4,3,8]
-strandSort(test_list)
+test_list_of_dicts = [{'first_name':'Sarah', 'last_name':'Martin', 'city':'NYC'}
+                       , {'first_name':'Rachel', 'last_name':'Martin', 'city':'Berlin'}
+                       , {'first_name':'David', 'last_name':'Martin', 'city':'Stamford'}]
+strandSort(test_list_of_dicts,'city')
 
 
 # GENERATES TEST DATA
@@ -153,21 +162,51 @@ len(first_names) == len(set(first_names))
 len(last_names) == len(set(last_names))
 # confirmed!
 
-# Combines lists into dictionary and then converts to pandas dataframe
+# Combines lists into dictionary and then converts to list of dictionaries
 persons_dict = {'first_name':first_names, 'last_name':last_names, 'address':states}
-persons_df = pd.DataFrame(persons_dict)
-persons_df
+persons_list = pd.DataFrame(persons_dict).to_dict('records')
 
 
 # TESTS 1. QUICK SORT, 2. BUBBLE SORT, 3. GNOME SORT, 4. INSERTION SORT, 5. SELECTION SORT, AND 6. STRAND SORT
+# due to in-place sorting, I'll create copies of the test data for each sort function
+persons_list_quick = persons_list
+persons_list_bubble = persons_list
+persons_list_gnome = persons_list
+persons_list_insertion = persons_list
+persons_list_selection = persons_list
+persons_list_strand = persons_list
 
-# # # LEFTOVER FROM PROF STARTER CODE
-# # Creates test person object with username, fav number, and interest attributes
-# person_obj = Person_pb2.Person()
-# person_obj.user_name = 'Martin'
-# person_obj.favorite_number = 1337
-# person_obj.interests.extend(['daydreaming','hacking'])
+# instantiates list to capture time test results
+time_results = []
 
-# # Not 100% sure yet what this does but i'll figure it out!
-# binascii.hexlify(person_obj.SerializeToString())
-# person_obj.SerializeToString()
+start = time.perf_counter()
+quicksort(persons_list_quick,'last_name')
+end = time.perf_counter()
+time_results.append({'Method':'Quick Sort', 'Time':(end - start)*1000})
+
+start = time.perf_counter()
+bubbleSort(persons_list_bubble,'last_name')
+end = time.perf_counter()
+time_results.append({'Method':'Bubble Sort', 'Time':(end - start)*1000})
+
+start = time.perf_counter()
+gnomeSort(persons_list_gnome,'last_name')
+end = time.perf_counter()
+time_results.append({'Method':'Gnome Sort', 'Time':(end - start)*1000})
+
+start = time.perf_counter()
+insertionSort(persons_list_insertion,'last_name')
+end = time.perf_counter()
+time_results.append({'Method':'Insertion Sort', 'Time':(end - start)*1000})
+
+start = time.perf_counter()
+selectionSort(persons_list_selection,'last_name')
+end = time.perf_counter()
+time_results.append({'Method':'Selection Sort', 'Time':(end - start)*1000})
+
+start = time.perf_counter()
+strandSort(persons_list_strand,'last_name')
+end = time.perf_counter()
+time_results.append({'Method':'Strand Sort', 'Time':(end - start)*1000})
+
+results_df = pd.DataFrame(time_results)
