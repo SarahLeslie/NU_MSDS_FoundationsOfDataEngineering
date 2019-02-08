87874abf0c9558741764d2f6fc3aea91e4faa11c
Sarah Martin <sarahlesliem21@gmail.com>
2019-02-07 20:09:47 -0500

    Time testing

--- week5_miniProgramming.py
+++ week5_miniProgramming.py
@@ -2,7 +2,7 @@
 import random
 import string
 import time
-# import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -102,65 +102,49 @@ def binary_search(list, item):
 
 
 # Now search for these six names in each of the collections.  
-# Use linear search for the unsorted list, 
-# binary search for the sorted list, 
-# and use the set.remove() builtin for the set.  
-# Capture the time it takes using all three algorithms.
-
-# # TESTS 1. QUICK SORT, 2. BUBBLE SORT, 3. GNOME SORT, 4. INSERTION SORT, 5. SELECTION SORT, AND 6. STRAND SORT
-# due to in-place sorting or elimination of the original data,
-# I'll create copies of the test data for each sort function for 2 sorts
-persons_list_quick1 = persons_list.copy()
-persons_list_bubble1 = persons_list.copy()
-persons_list_gnome1 = persons_list.copy()
-persons_list_insertion1 = persons_list.copy()
-persons_list_selection1 = persons_list.copy()
-persons_list_strand1 = persons_list.copy()
-
-persons_list_quick2 = persons_list.copy()
-persons_list_bubble2 = persons_list.copy()
-persons_list_gnome2 = persons_list.copy()
-persons_list_insertion2 = persons_list.copy()
-persons_list_selection2 = persons_list.copy()
-persons_list_strand2 = persons_list.copy()
-
+# # TESTING
 # instantiates list to capture time test results
 time_results = []
 
-def time_testing(sort_method,method_label,data1,data2):
-  start1 = time.perf_counter()
-  by_last_name = sort_method(data1,'last_name')
-  end1 = time.perf_counter()
-  start2 = time.perf_counter()
-  by_address = sort_method(data2,'address')
-  end2 = time.perf_counter()
-  first_sort_time = (end1 - start1)*1000
-  second_sort_time = (end2 - start2)*1000
-  time_results.append({'Method':method_label
-                      ,'Sort Key':'Last Name'
-                      ,'Sort Time':first_sort_time})
-  time_results.append({'Method':method_label
-                      ,'Sort Key':'Address'
-                      ,'Sort Time':second_sort_time})
-  time_results.append({'Method':method_label
-                      ,'Sort Key':'Average'
-                      ,'Sort Time':(first_sort_time + second_sort_time)/2})
-  return by_last_name, by_address
+# defines 2 functions to time test search method (1 for the lists, 1 for the set)
+def list_time_testing(search_function,method_label,test_list,search_for_list):
+    indeces = [9999, 29999, 49999, 69999, 89999, 99999]
+    for i in range(len(search_for_list)):
+        item = search_for_list[i]
+        start = time.perf_counter()
+        search_function(test_list,item)
+        end = time.perf_counter()
+        search_time = (end - start)*1000
+        time_results.append({'Search Function':method_label
+                              ,'Index':indeces[i]
+                              ,'Search Time':search_time})
 
-time_testing(quickSort,'Quick Sort',persons_list_quick1,persons_list_quick2)
-time_testing(bubbleSort,'Bubble Sort',persons_list_bubble1,persons_list_bubble2)
-time_testing(gnomeSort,'Gnome Sort',persons_list_gnome1,persons_list_gnome2)
-time_testing(insertionSort,'Insertion Sort',persons_list_insertion1,persons_list_insertion2)
-time_testing(selectionSort,'Selection Sort',persons_list_selection1,persons_list_selection2)
-time_testing(strandSort,'Strand Sort',persons_list_strand1,persons_list_strand2)
+def set_time_testing(test_set,search_for_list):
+  for i in range(len(search_for_list)):
+      item = search_for_list[i]
+      start = time.perf_counter()
+      test_set.remove(item)
+      end = time.perf_counter()
+      search_time = (end - start)*1000
+      time_results.append({'Search Function':"Set '.remove()'"
+                          ,'Index':'NA'
+                          ,'Search Time':search_time})
 
+# conducts the time testing
+list_time_testing(linear_search,'Linear Search for Unsorted List',unsorted_list,search_test_names['unsorted_list'])
+list_time_testing(binary_search,'Binary Search for Sorted List',sorted_list,search_test_names['sorted_list'])
+set_time_testing(unsorted_set,search_test_names['unsorted_list'])
+set_time_testing(unsorted_set,search_test_names['sorted_list'])
+
+# organizes results into pandas dataframe for easier exploration
 results_df = pd.DataFrame(time_results)
+results_df['Index' == 'NA', 'Search Time']
 
-sns.catplot(x="Sort Key", y="Sort Time"
-            , hue = "Method", data=results_df)
-plt.show()
+# sns.catplot(x="Sort Key", y="Sort Time"
+#             , hue = "Method", data=results_df)
+# plt.show()
 
-sns.catplot(x="Sort Key", y="Sort Time"
-            , hue = "Method", jitter = False
-            , data=results_df.loc[~results_df['Method'].isin(['Gnome Sort','Bubble Sort'])])
-plt.show()
+# sns.catplot(x="Sort Key", y="Sort Time"
+#             , hue = "Method", jitter = False
+#             , data=results_df.loc[~results_df['Method'].isin(['Gnome Sort','Bubble Sort'])])
+# plt.show()
