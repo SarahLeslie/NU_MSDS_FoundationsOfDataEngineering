5d7694ffdffe810e384abcb977951e1cce06d06c
Sarah Martin <sarahlesliem21@gmail.com>
2019-03-05 23:25:34 -0500

    greedy method implemented

--- week9_miniProgramming.py
+++ week9_miniProgramming.py
@@ -2,7 +2,7 @@
 
 # IMPORTS REQUIRED PACKAGES
 import random
-import json
+import copy
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -47,26 +47,88 @@ sum(cases['case_2']['items'][x]['weight'] for x in range(len(cases['case_2']['it
 
 
 # DEFINES KNAPSACK SOLUTION FUNCTIONS
-# recursive method
-def recursive_knapsack(items, weight_capacity):
-    return True
-
-# testing recursive method
-recursive_knapsack(test_items, 10)
-
 # greedy method
-def greedy_knapsack(items, weight_capacity):
-    return True
+def greedy_knapsack(case):
+    bag = []
+    weight_available = case['bag_weight_capacity']
+    while weight_available > 0:
+        new_subset = [x for x in case['items'] if x['weight'] <= weight_available]
+        if not new_subset:
+            return bag
+        current_max_value = max([x['value'] for x in new_subset])
+        items_with_max_value = [x for x in new_subset if x['value'] == current_max_value]
+        min_weight_among_max_value_items = min([x['weight'] for x in items_with_max_value])
+        item_to_pop_index = [x for x in range(len(case['items'])) if (case['items'][x]['value'] == current_max_value \
+            and case['items'][x]['weight'] == min_weight_among_max_value_items)][0]
+        weight_available = weight_available - case['items'][item_to_pop_index]['weight']
+        bag.append(case['items'].pop(item_to_pop_index))
+    return bag
 
 # testing greedy method
-greedy_knapsack(test_items, 10)
+single_test_case = copy.deepcopy(cases['case_1'])
+result_bag = greedy_knapsack(single_test_case)
+single_test_case['bag_weight_capacity']
+sum(x['weight'] for x in result_bag)
+sum(x['value'] for x in result_bag)
+
+
+# recursive method
+def recursive_knapsack(items, weight_capacity):
+    
+    return True
+
+int knapsack(int w[], int p[], int n, int M)
+{
+    //In every pass, we can either include nth item or not
+    //if the capacity of knapsack is left to NIL, no value can be attained
+    if(M==0)
+        return 0;
+    //if no more items are left, no value can be attained
+    if(n==0)
+        return 0;
+    //if current item, weighs more than the capacity of knapsack, it can not be included
+    if(w[n-1]>M)
+        return knapsack(w,p,n-1,M);
+    //else select the maximum value of once including the current item and once not including it
+    return max(knapsack(w,p,n-1,M),p[n-1]+knapsack(w,p,n-1,M-w[n-1]));
+}
+int main()
+{
+    int i,n;
+    int M;  //capacity of knapsack
+    cout<<"Enter the no. of items ";
+    cin>>n;
+    int w[n];  //weight of items
+    int p[n];  //value of items
+    cout<<"Enter the weight and price of all items"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cin>>w[i]>>p[i];
+    }
+    cout<<"enter the capacity of knapsack  ";
+    cin>>M;
+    cout<<"The maximum value of items that can be put into knapsack is "<<knapsack(w,p,n,M);
+    return 0;
+}
+
+# testing recursive method
+single_test_case = copy.deepcopy(cases['case_1'])
+result_bag = recursive_knapsack(single_test_case)
+single_test_case['bag_weight_capacity']
+sum(x['weight'] for x in result_bag)
+sum(x['value'] for x in result_bag)
+
 
 # dynamic programming method
 def dp_knapsack(items, weight_capacity):
     return True
 
 # testing dynamic programming
-dp_knapsack(test_items, 10)
+single_test_case = copy.deepcopy(cases['case_1'])
+result_bag = dp_knapsack(single_test_case)
+single_test_case['bag_weight_capacity']
+sum(x['weight'] for x in result_bag)
+sum(x['value'] for x in result_bag)
 
 
 # TIME TESTING
